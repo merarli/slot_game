940fbea84bc4eed2819ae561b1e4a54580742d59
merarli <merarli@Merarli12.local>
2017-11-28 16:36:52 +0900

    画像クリックで変更、真ん中はRANDOMで変更

--- function.js
+++ function.js
@@ -13,25 +13,13 @@ img[3] = new Image();
 img[3].src = "img/slot4.jpg";
 img[4] = new Image();
 img[4].src = "img/slot5.jpg";
-
-var timer_id;
-var mawasu = function () {
-    //1000ミリ秒（1秒）毎に関数「showNowDate()」を呼び出す
-    timer_id = setInterval("changeIMGb()", 100);
-    setInterval("changeIMGa()", 100);
-    setInterval("changeIMGc()", 100);
-
-}
-
-
-
-
 var cnt = 0;
+
 //画像切り替え関数 
 function changeIMGa() {
 
 //画像番号を進める 
-    if (cnt == 4) {
+    if (cnt === 4) {
         cnt = 0;
     } else {
         cnt++;
@@ -43,25 +31,8 @@ function changeIMGa() {
 
 //画像切り替え関数 
 function changeIMGb() {
-//var cnt = 0;
-//画像番号を進める 
-//    var cnt = Math.floor(Math.random() * 5);
-//    if (cnt == 4) {
-//        cnt = 0;
-//    } else {
-//        cnt++;
-//    }
-
-//画像を切り替える 
-
     var cnt = Math.floor(Math.random() * 5);
     document.getElementById("gazoB").src = img[cnt].src;
-
-
-}
-
-var foo = function(){
-    setInterval(changeIMGb(), 100);
 }
 
 
@@ -69,7 +40,7 @@ var foo = function(){
 function changeIMGc() {
 //var cnt = 0;
 //画像番号を進める 
-    if (cnt == 4) {
+    if (cnt === 4) {
         cnt = 0;
     } else {
         cnt++;
@@ -77,8 +48,4 @@ function changeIMGc() {
 
 //画像を切り替える 
     document.getElementById("gazoC").src = img[cnt].src;
-}
-
-function botton(){
-    foo();
 }
